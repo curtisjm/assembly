f6de84e43727aaacc0240f66e6b2d60fabad1139
Curtis Mitchell <curtisjmitchell1@gmail.com>
2021-08-25 22:01:23 -0700

    add first hw

---
+++ number-systems-converter/main.cpp
@@ -0,0 +1,16 @@
+// Name: Curtis Mitchell
+// Date: 8/25/21
+// Compiler: MinGW-w64
+// Assignment: 
+
+#include <iostream>
+
+using namespace std;
+
+string convertBase(int fromBase, int toBase, string value) {
+
+}
+
+int main() {
+
+}
