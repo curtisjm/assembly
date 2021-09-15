19fd0d4ce67ee9ec7d09b7c650daf395c3a55008
Curtis Mitchell <curtisjmitchell1@gmail.com>
2021-09-15 00:09:22 -0700

    finish svm

--- classification/support-vector-machines/svm_from_scratch.py
+++ classification/support-vector-machines/svm_from_scratch.py
@@ -20,7 +20,7 @@ class SupportVectorMachine:
         opt_dict = {}
 
         # use transforms to get rid of negatives in dot products
-        transforms = [[1, 1], [-1, -1], [-1, -1], [1, -1]]
+        transforms = [[1, 1], [-1, 1], [-1, -1], [1, -1]]
 
         all_data = []
         for yi in self.data:
@@ -41,7 +41,7 @@ class SupportVectorMachine:
         ]
 
         # b doesn't need to have a small of a step size as w, doesn't have to be as precise
-        b_range_multiple = 5
+        b_range_multiple = 2
         # we don't need to take as small steps with b
         b_multiple = 5
         # first element in vector w
@@ -66,7 +66,7 @@ class SupportVectorMachine:
                         for i in self.data:
                             for xi in self.data[i]:
                                 yi = i
-                                if not yi * (np.dot(w_t, xi + b)) >= 1:
+                                if not yi * (np.dot(w_t, xi) + b) >= 1:
                                     found_option = False
                         if found_option:
                             opt_dict[np.linalg.norm(w_t)] = [w_t, b]
@@ -87,13 +87,73 @@ class SupportVectorMachine:
             self.b = opt_choice[1]
             latest_optimum = opt_choice[0][0] + step * 2
 
+            for i in self.data:
+                for xi in self.data[i]:
+                    yi = i
+                    print(xi, ":", yi * (np.dot(self.w, xi) + self.b))
+
     def predict(self, features):
         # sign(xw + b)
         classification = np.sign(np.dot(np.array(features), self.w) + self.b)
+        if classification != 0 and self.visualization:
+            self.ax.scatter(
+                features[0],
+                features[1],
+                s=200,
+                marker="*",
+                c=self.colors[classification],
+            )
         return classification
+
+    def visualize(self):
+        for i in data_dict:
+            for x in data_dict[i]:
+                self.ax.scatter(x[0], x[1], s=100, c=self.colors[i])
+
+        # hyperplane = x.w + b
+        # v = x.w + b
+        # pos SV = 1
+        # neg SV = -1
+        # decision boundary = 0
+        def hyperplane(x, w, b, v):
+            return (-w[0] * x - b + v) / w[1]
+
+        datarange = (self.min_feature_value * 0.9, self.max_feature_value * 1.1)
+        hyp_x_min = datarange[0]
+        hyp_x_max = datarange[1]
+
+        # w.x + b = 1
+        # positive support vector hyperplane
+        psv1 = hyperplane(hyp_x_min, self.w, self.b, 1)
+        psv2 = hyperplane(hyp_x_max, self.w, self.b, 1)
+        self.ax.plot([hyp_x_min, hyp_x_max], [psv1, psv2], "k")
+
+        # w.x + b = -1
+        # negative support vector hyperplane
+        nsv1 = hyperplane(hyp_x_min, self.w, self.b, -1)
+        nsv2 = hyperplane(hyp_x_max, self.w, self.b, -1)
+        self.ax.plot([hyp_x_min, hyp_x_max], [nsv1, nsv2], "k")
+
+        # w.x + b = 0
+        # decision boundary
+        db1 = hyperplane(hyp_x_min, self.w, self.b, 0)
+        db2 = hyperplane(hyp_x_max, self.w, self.b, 0)
+        self.ax.plot([hyp_x_min, hyp_x_max], [db1, db2], "y--")
+
+        plt.show()
 
 
 data_dict = {
     -1: np.array([[1, 7], [2, 8], [3, 8]]),
     1: np.array([[5, 1], [6, -1], [7, 3]]),
 }
+
+svm = SupportVectorMachine()
+svm.fit(data=data_dict)
+
+predict_us = [[0, 10], [1, 3], [3, 4], [3, 5], [5, 5], [5, 6], [6, -5], [5, 8]]
+
+for p in predict_us:
+    svm.predict(p)
+
+svm.visualize()
